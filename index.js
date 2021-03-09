9974098213a196fac4285f2fcd06e9dd09aa83fc
Gina Vargas <gina.vargas89@outlook.com>
2021-03-08 23:12:05 -0800

    adding readme info for page

--- index.js
+++ index.js
@@ -2,48 +2,92 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-const readMe = generateReadMe ();
+const README = (response) => 
+`# ${response.title}
+## Table of Contents
+*[Description](#description)
+*[Installation](#installation)
+*[Usage](#usage)
+*[License](#license)
+*[Contribution](#contribution)
+*[Tests](#tests)
+*[Questions](#questions)
+## Description
+${response.description}
+## Installation
+${response.install}
+## Usage
+${response.usage}
+## License
+This application has the following License: ${response.license}
+## Contributing 
+${response.contributing}
+## Tests
+${response.test}
+`;
 
 inquirer.prompt([
     {
-        message: "What is your GitHub username?",
-        name: "username"
+        type: 'input',
+        name: 'title',
+        message: "What would you like your title to be?"
     },
     {
-        message: "What is your email address?",
-        name: "email address"
+        type: 'input',
+        name: 'decription',
+        message: 'Please write a short description of your project.'
     },
     {
-        message: "What is the name of the project?",
-        name: "title"
+        type: 'input',
+        name: 'install',
+        message: 'Add any installation instructions.'
     },
     {
-        message: "Please write a short description of your project",
-        name: "description"
+        type: 'input',
+        name: 'usage',
+        message: 'Add usage information.'
     },
     {
-        message: "What kind of license should your project have?",
-        name: "MIT"
+        type: 'input',
+        name: 'contribution',
+        message: 'Add contribution guidelines.'
     },
     {
-        message: "What command should be to install dependencies?",
-        name: "npm 1"
+        type: 'input',
+        name: 'test',
+        message: 'Add test instructions.'
     },
     {
-        message: "What command should be run to run tests?",
-        name: "npm test"
+        type: 'input',
+        name: 'license',
+        message: 'Would you like to add a license?',
+        choices: ['None', 'Apache', 'MIT', 'BSD']
     },
     {
-        message: "What does the user need to know about using the repo?",
-        name: "node index.js"
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
     },
     {
-        message: "What does the user need to know about contributing to the repo?",
-        name: "answer"
+        type: 'input',
+        name: 'location',
+        message: 'Where do you live?'
     },
+    {
+        type: 'input',
+        name: 'linkd',
+        message: 'What is your LinkedIn username?'
+    },
+    {
+        type: 'input',
+        name: 'githubname',
+        message: 'What is your GitHub username?'
+    },
+
 ])
 .then ((response) => {
-    const readMeContent = generateReadMe(response);
+   
+    console.log(response)
 
       fs.writeFile('README.md', (err) => 
        err ? console.log(err) : console.log('ReadMe Generator Worked!')  
